1af2557703ef2e70bc12c51f5512edb4fb29b251
Vincent Huang <vincent.huang@wework.com>
2018-02-09 01:07:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz
+  output = ""
   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    output += "Fizz" # Go fizz
+  elsif int % 5 == 0
+    output += "Buzz"
   end
 end
